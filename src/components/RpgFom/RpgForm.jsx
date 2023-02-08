9931c3ed6903b9ab91c89dd9044c1e861968a7f4
julia <julia.albq@gmail.com>
2023-02-08 17:21:16 -0300

    feat: add RpgForm component

---
+++ src/components/RpgFom/RpgForm.jsx
@@ -0,0 +1,56 @@
+const classes = ['artificer', 'barbarian', 'bard', 'cleric', 'druid', 'fighter', 'monk', 'paladin', 'ranger', 'rogue', 'sorcerer', 'warlock', 'wizard']
+
+export const RpgForm = ({name, 
+handleName,
+lastName,
+handleLastName,
+xp,
+handleXp,
+level,
+handleLevel,
+mana,
+handleMana,
+life,
+handleLife,
+characterClass,
+handleClass}) => {
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label name='name'>
+          Name:
+        <input id='name' placeholder="Name" value={name} onChange={handleName}/>
+        </label>
+        <label name='lastName'>
+          Last name:
+          <input id='lastName' placeholder="Last name" value={lastName} onChange={handleLastName}/>
+        </label>
+        <label name='level'>
+          Level:
+          <input type='number' id='level' placeholder="Level" value={level} onChange={handleLevel}/>
+        </label>
+        <label name='xp'>
+          XP:
+          <input type='number' id='xp' placeholder="XP" value={xp} onChange={handleXp}/>
+        </label>
+        <label name='life'>
+          Life:
+          <input type='number' id='life' placeholder="Life" value={life} onChange={handleLife}/>
+        </label>
+        <label name='mana'>
+          Mana:
+          <input type='number' id='mana' placeholder="Mana" value={mana} onChange={handleMana}/>
+        </label>
+        <label name='characterClass'>
+          Class:
+          <select name="characterClass" id="characterClass" value={characterClass} onChange={handleClass}>
+            <option value="">Select a class</option>
+            {classes.map((characterClass) => <option key={characterClass} value={characterClass}>{characterClass}</option>)}
+          </select>
+        </label>
+        <button type="submit">Save</button>     
+      </form>
+    </div>
+  )
+}
