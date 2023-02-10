b5e946cfe3f59c5287f9535917aaccccd8f439c6
julia <julia.albq@gmail.com>
2023-02-10 10:00:28 -0300

    feat: add class icon to the Identification component

--- src/components/Identification/Identification.jsx
+++ src/components/Identification/Identification.jsx
@@ -1,14 +1,11 @@
 import './Identification.css'
 
-//const icons = [artificer, barbarian, bard, cleric, druid, fighter, monk, paladin, ranger, rogue, sorcerer, warlock, wizard]
-
 export const Identification = ({name, lastName, characterClass}) => {
 
-  const iconChoice = `%PUBLIC_URL%/classIcons/${characterClass}.jpeg` 
-
   return (
-    <div>
-      <h2>Character name: {name} {lastName}</h2>  
+    <div className='identification'>
+      <h2>Character name: {name} {lastName}</h2> 
+      <img src={`${process.env.PUBLIC_URL}/classIcons/${characterClass}.jpeg`} alt="" />
     </div>
   )
 }
